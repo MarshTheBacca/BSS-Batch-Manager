b3d8152395a3c6945a056ef27ac233bb23ac6506
MarshTheBacca <marshallhunt@live.com>
2023-12-10 16:09:37 +0000

    child_process moved to globally defined for windows multiprocessing compatability

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -7,7 +7,8 @@ from subprocess import Popen
 from .validation_utils import micro_valid_int
 from .file_utils import linux_path_converter
 
-def find_char_indexes(string, target_char, invert = 0):
+
+def find_char_indexes(string, target_char, invert=0):
     matching_indexes = []
     for i, char in enumerate(string):
         if char == target_char and invert == 0:
@@ -16,7 +17,8 @@ def find_char_indexes(string, target_char, invert = 0):
             matching_indexes.append(i)
     return matching_indexes
 
-def clean_name(string,conversions = {"(":"", ")":"", "^":"pow", " ":"_"}):
+
+def clean_name(string, conversions={"(": "", ")": "", "^": "pow", " ": "_"}):
     return_string = ""
     for char in string:
         if char in conversions:
@@ -30,19 +32,17 @@ def job_script_maker(template_file_path,
                      save_path,
                      remote_job_path,
                      exec_name,
-                     job_desc = "qsub_log",
-                     job_path_line = 9,
-                     exec_line = 15,
-                     file_name = "job_submission_script.sh"):
-    
+                     job_desc="qsub_log",
+                     job_path_line=9,
+                     exec_line=15,
+                     file_name="job_submission_script.sh"):
+
     remote_job_path = linux_path_converter(remote_job_path)
     with open(template_file_path, "r") as file:
         template_lines = file.read().split("\n")
-        
     template_lines[6] = f"#$ -N {job_desc}"
-    template_lines[job_path_line-1] = f'job_dir="{remote_job_path}"'
+    template_lines[job_path_line - 1] = f'job_dir="{remote_job_path}"'
     template_lines[exec_line - 1] = f"./{exec_name}"
-    
     with open(os.path.join(save_path, file_name), "w+") as file:
         for line in template_lines:
             file.write(f"{line}\n")
@@ -65,13 +65,14 @@ def get_batch_IDs(string, lower, upper):
             return [int(string)]
     return False
 
+
 def get_seed(cwd, job_path, seeds_path, seed_size):
     seeds = [int(seed.name) for seed in os.scandir(seeds_path) if seed.is_dir()]
     new_seed_path = os.path.join(seeds_path, str(seed_size))
     if seed_size not in seeds:
         os.mkdir(new_seed_path)
         os.chdir(new_seed_path)
-        Popen(["python",os.path.join(seeds_path, "make_poly_seed.py"),str(seed_size)]).wait()
+        Popen(["python", os.path.join(seeds_path, "make_poly_seed.py"), str(seed_size)]).wait()
         os.chdir(cwd)
     copytree(new_seed_path, os.path.join(job_path, "seed"))
 
@@ -83,19 +84,15 @@ def get_ip():
     s.close()
     return ip
 
-def background_process(command_array, silent = True):
-    def child_process(command_array, silent = True):
-        if silent:
-            subprocess.Popen(command_array, start_new_session = True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
-        else:
-            subprocess.Popen(command_array, start_new_session = True)
-    process = multiprocessing.Process(target = child_process, args = [command_array])
+
+def background_process(command_array, silent=True):
+    process = multiprocessing.Process(target=child_process, args=[command_array])
     process.daemon = True
     process.start()
 
 
-
-
-
-
-
+def child_process(command_array, silent=True):
+    if silent:
+        subprocess.Popen(command_array, start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    else:
+        subprocess.Popen(command_array, start_new_session=True)
