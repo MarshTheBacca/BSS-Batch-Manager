ac7fb791b2f37371b3e0304f4613fe7e04cbc3ec
MarshTheBacca <marshallhunt@live.com>
2023-12-11 02:46:18 +0000

    pep8 adherence

--- utils/config.py
+++ utils/config.py
@@ -5,18 +5,20 @@ from zipfile import ZipFile
 from .other_utils import find_char_indexes, clean_name
 from .validation_utils import valid_int, valid_triple, valid_csv
 from .array_utils import import_2D
-dashes="--------------------------------------------------"
-program_type_constants={"netmc":{"template_file_name":"netmc_template.csv",
-                                 "file_name":"netmc.inpt"},
-                        
-                        "netmc_pores":{"template_file_name":"netmc_pores_template.csv",
-                                       "file_name":"netmc.inpt"},
 
-                        "triangle_raft":{"template_file_name":"triangle_raft_template.csv",
-                                         "file_name":"mx2.inpt"}}
+dashes = "--------------------------------------------------"
+program_type_constants = {"netmc": {"template_file_name": "netmc_template.csv",
+                                    "file_name": "netmc.inpt"},
+                          "netmc_pores": {"template_file_name": "netmc_pores_template.csv",
+                                          "file_name": "netmc.inpt"},
+                          "triangle_raft": {"template_file_name": "triangle_raft_template.csv",
+                                            "file_name": "mx2.inpt"}}
 
-modes = {"ses":"Start, End, Step", "sen":"Start, End, Number of Steps","nums":"Num1, Num2...","any_string":"Any String",
-         "selected_strings":"Selected Strings", "bool":"Boolean, 0 or 1"}
+modes = {"ses": "Start, End, Step", "sen": "Start, End, Number of Steps",
+         "nums": "Num1, Num2...", "any_string": "Any String",
+         "selected_strings": "Selected Strings", "bool": "Boolean, 0 or 1"}
+
+
 class Config:
     def __init__(self, prog_type):
         self.type = prog_type
@@ -27,7 +29,7 @@ class Config:
     def import_config(self):
         def convert_config_array(array):
             return_array = []
-            for i in range(0,len(array)):
+            for i in range(0, len(array)):
                 array[i].append(None)
                 array[i] = [None if element == "None" else True if element == "True" else False
                             if element == "False" else element for element in array[i]]
@@ -38,22 +40,21 @@ class Config:
                     if ";" in array[i][7]:
                         array[i][7] = array[i][7].split(";")
                 if "dual" in array[i][5]:
-                      first_num, second_num = extract_dual(array[i][1])
-                      second_line = [array[i][0], second_num, array[i][2], array[i][3],
-                                     array[i][4], array[i][5], array[i][6][1], array[i][7][1], True]
-                      array[i][1], array[i][6], array[i][7], array[i][8] = first_num, array[i][6][0], array[i][7][0], False
-                      return_array.extend([array[i], second_line])
+                    first_num, second_num = extract_dual(array[i][1])
+                    second_line = [array[i][0], second_num, array[i][2], array[i][3],
+                                   array[i][4], array[i][5], array[i][6][1], array[i][7][1], True]
+                    array[i][1], array[i][6], array[i][7], array[i][8] = first_num, array[i][6][0], array[i][7][0], False
+                    return_array.extend([array[i], second_line])
                 else:
                     return_array.append(array[i])
-                      
             return return_array
-                    
+
         def extract_dual(string):
             space_indexes = find_char_indexes(string, " ")
             first_num = string[0:space_indexes[0]]
             second_num = string[space_indexes[-1] + 1:]
             return first_num, second_num
-        
+
         array = import_2D(self.csv_file, (0,))
         array = convert_config_array(array)
         self.variables = []
@@ -63,14 +64,14 @@ class Config:
                 self.variables.append(Config.Var(row[0], row[1], row[3], row[4], row[5], row[6], row[7], row[8]))
             else:
                 self.fillers.append(Config.Filler(row[0], row[1]))
-    
+
     def export_config_string(self):
         combined = sorted(self.variables + self.fillers)
         string = ""
         for element in combined:
-            if type(element) == Config.Filler:
-                string += element.string+"\n"
-            elif type(element) == Config.Var:
+            if element.isinstance(Config.Filler):
+                string += element.string + "\n"
+            elif element.isinstance(Config.Var):
                 if element.type in ("int", "float", "str", "bool"):
                     if element.valid_func == float:
                         string += str(round(element.value, 5)) + "\t\t" + element.name + "\n"
@@ -82,7 +83,6 @@ class Config:
                     if not element.is_second:
                         if element.valid_func == float:
                             string += str(round(element.value, 5)) + "\t"
-                            
                         else:
                             string += str(element.value) + "\t"
                     else:
@@ -91,7 +91,7 @@ class Config:
                         else:
                             string += str(element.value) + "\t" + element.comb_name + "\n"
         return string.expandtabs(8)
-            
+
     def generate_batch(self, cvas, var_indexes, save_path, batch_name):
         meshgrid = np.array(np.meshgrid(*cvas)).T.reshape(-1, len(cvas))
         temp_config = deepcopy(self)
@@ -105,7 +105,7 @@ class Config:
                         job_name += f"{changing_vars[k].name}_{int(value)}__"
                     else:
                         changing_vars[k].value = value
-                        job_name += f"{changing_vars[k].name}_{round(value,3)}__"
+                        job_name += f"{changing_vars[k].name}_{round(value, 3)}__"
                 job_name = clean_name(job_name[:-2])
                 if self.type == "netmc":
                     temp_config.variables[0].value = job_name
@@ -116,12 +116,12 @@ class Config:
 
     def table(self):
         return [[var.value, var.name, var.type, var.allowed_values_string] for var in self.variables if var.relevant], [i for i in range(0, len(self.variables)) if self.variables[i].relevant]
-        
+
     class Var:
         def __init__(self, line, value, relevant, name, var_type, indiv_name, allowed_values, is_second):
             self.line = int(line)
             if var_type == "int/str":
-                self.value, self.value_str, self.valid_func = Config.Var.convert_value(value, var_type)    
+                self.value, self.value_str, self.valid_func = Config.Var.convert_value(value, var_type)
             else:
                 self.value, self.valid_func = Config.Var.convert_value(value, var_type)
             if indiv_name:
@@ -133,7 +133,7 @@ class Config:
             self.relevant = relevant
             self.type = var_type
             self.allowed_values, self.allowed_values_string, self.modes = Config.Var.allowed(allowed_values, var_type, self.valid_func)
-            
+
         def convert_value(value, var_type):
             if var_type == "str":
                 return value, None
@@ -163,10 +163,10 @@ class Config:
                 else:
                     lower = float("-inf")
                     upper = float("inf")
-                return lower, upper        
-            
+                return lower, upper
+
             if var_type == "str":
-                if type(allowed_values) == list:
+                if allowed_values.isinstance(list):
                     allowed_values_string = ""
                     for string in allowed_values:
                         allowed_values_string += string + ", "
@@ -188,12 +188,12 @@ class Config:
             string = ""
             exit_num = len(self.modes) + 1
             for i, mode in enumerate(self.modes):
-                string += str(i + 1) + ") "+ modes[mode] + "\n"
+                string += str(i + 1) + ") " + modes[mode] + "\n"
             print(f"{string}{exit_num}) Exit\n")
             option = valid_int("How would you like to vary this variable?\n", 1, exit_num)
             if option == exit_num:
                 return False, None
-            mode = self.modes[option-1]
+            mode = self.modes[option - 1]
             if mode == "ses":
                 while True:
                     prompt = "Enter your start, end, step string in the form: s,e,s\n"
@@ -214,10 +214,10 @@ class Config:
                         break
                 if self.valid_func == int:
                     cva = np.round(np.linspace(triple[0], triple[1], triple[2]), 0)
-                    print(f"The increment will be roughly: {round(cva[1]-cva[0], 0)}")
+                    print(f"The increment will be roughly: {round(cva[1] - cva[0], 0)}")
                 else:
                     cva = np.round(np.linspace(triple[0], triple[1], triple[2]), 5)
-                    print(f"The increment will be: {round(cva[1]-cva[0], 5)}")
+                    print(f"The increment will be: {round(cva[1] - cva[0], 5)}")
                 if self.valid_func == int:
                     return True, np.int_(np.round_(cva))
                 else:
@@ -246,22 +246,18 @@ class Config:
             elif mode == "bool":
                 return True, [0, 1]
 
-        
-        
         def __str__(self):
             if self.type in ("str", "bool", "dual bool"):
                 return f"{self.line}\t\t{self.name}\t\t{self.value}\t\t{self.type}\t\t{self.accepted}"
             elif self.type in ("int", "float", "dual int", "dual float", "int/str"):
                 return f"{self.line}\t\t{self.name}\t\t{self.value}\t\t{self.type}\t\t{self.lower}\t\t{self.upper}"
-            
+
         def __lt__(self, other):
             if self.line == other.line:
                 if self.is_second:
                     return False
             else:
-                return self.line < other.line            
-        
-        
+                return self.line < other.line
 
     class Filler:
         def __init__(self, line, string):
@@ -270,10 +266,9 @@ class Config:
                 self.string = dashes
             else:
                 self.string = string
-            
+
         def __str__(self):
             return f"{self.line}\t\t{self.string}"
-        
+
         def __lt__(self, other):
             return self.line < other.line
-                        
