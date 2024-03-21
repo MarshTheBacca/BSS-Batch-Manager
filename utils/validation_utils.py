438c1db75795d5383233871553cd8de77f469075
MarshTheBacca <marshallhunt@live.com>
2024-03-21 20:52:58 +0000

    optimised get_valid_int and get_valid_str. Added doc strings, removed valid_triple and valid_csv, as their functionality is now built in to variation modes class

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -1,24 +1,49 @@
-from typing import Callable, Tuple, Optional
+from typing import Tuple, Optional
 import sys
 
 
-def get_valid_int(prompt: str, lower: float | int = float("-inf"), upper: float | int = float("inf"),
-                  confirm_num: Optional[int] = None) -> int:
+def get_valid_int(prompt: str, lower: float | int = float("-inf"), upper: float | int = float("inf"), exit_string: Optional[str] = None) -> int | None:
+    """
+    Obtains a valid integer from the user within a given range
+
+    Args:
+        prompt (str): The prompt to display to the user
+        lower (float | int): The lower bound of the range
+        upper (float | int): The upper bound of the range
+        exit_string (str): The string to enter to exit the prompt
+
+    Returns:
+        int | None: The valid integer entered by the user, or None if the user entered the exit string
+    """
     while True:
+        answer = input(prompt)
+        if answer == exit_string:
+            return None
         try:
-            answer = int(input(prompt))
+            answer = int(answer)
         except ValueError:
             print("Answer is not a valid integer")
             continue
         if answer < lower or answer > upper:
             print(f"Answer is out of bounds, must be between {lower} and {upper} inclusive")
             continue
-        if confirm_num is None or (answer == confirm_num and confirm()):
-            return answer
+        return answer
 
 
 def valid_int(string: str, lower: float | int = float("-inf"), upper: float | int = float("inf"),
               verbose: bool = False) -> bool:
+    """
+    Checks if a string is a valid integer within a given range
+
+    Args:
+        string (str): The string to check
+        lower (float | int): The lower bound of the range
+        upper (float | int): The upper bound of the range
+        verbose (bool): Whether to print error messages
+
+    Returns:
+        bool: True if the string is a valid integer within the range, False otherwise
+    """
     try:
         int(string)
     except ValueError:
@@ -32,18 +57,41 @@ def valid_int(string: str, lower: float | int = float("-inf"), upper: float | in
     return True
 
 
-def get_valid_str(prompt: str, length_range: Tuple[int, int] = (0, sys.maxsize),
-                  char_types: Tuple[str, ...] | list[str] = (), verbose: bool = True) -> str:
+def get_valid_str(prompt: str,
+                  allowed_chars: Optional[list[str]] = None,
+                  forbidden_chars: Optional[list[str]] = None,
+                  lower: int = 0,
+                  upper: int = sys.maxsize,
+                  verbose: bool = True) -> str:
+    """
+    Gets a valid string from the user
+
+    Args:
+        prompt (str): The prompt to display to the user
+        allowed_chars (list[str]): A list of allowed characters
+        forbidden_chars (list[str]): A list of forbidden characters
+        lower (int): The minimum length of the string
+        upper (int): The maximum length of the string
+        verbose (bool): Whether to print error messages
+
+    Returns:
+        str: The valid string entered by the user
+    """
     while True:
         string = input(prompt)
-        if len(string) < length_range[0] or len(string) > length_range[1]:
+        if len(string) < lower or len(string) > upper:
             if verbose:
-                print(f"Input must be between {length_range[0]} and {length_range[1]} characters long")
+                print(f"Input must be between {lower} and {upper} characters long")
             continue
-        if all(char in char_types for char in string):
-            return string
-        if verbose:
-            print("Input contains invalid characters")
+        if allowed_chars and not set(string).issubset(allowed_chars):
+            if verbose:
+                print(f"Input must only contain {allowed_chars}")
+            continue
+        if forbidden_chars and set(string).intersection(forbidden_chars):
+            if verbose:
+                print(f"Input must not contain {forbidden_chars}")
+            continue
+        return string
 
 
 def valid_str(string: str, length_range: Tuple[int, int] = (0, sys.maxsize),
@@ -59,74 +107,6 @@ def valid_str(string: str, length_range: Tuple[int, int] = (0, sys.maxsize),
                 print(f"Input must not contain {char}")
             return False
     return True
-
-
-def valid_triple(prompt: str, trip_type: str, valid_func: Callable,
-                 lower: float | int = float("-inf"), upper: float | int = float("inf"),
-                 delim: str = ",", exit_string: str = "e") -> tuple:
-    nums = input(prompt)
-    if nums == exit_string:
-        return False, "exit"
-    nums = nums.split(delim)
-    if len(nums) != 3:
-        print("You must enter only 3 numbers")
-        return False, None
-    print(valid_func)
-    for i, num in enumerate(nums):
-        try:
-            nums[i] = valid_func(num)
-        except ValueError:
-            print("You did not enter either integers or floats")
-            return False, None
-    if nums[0] > nums[1]:
-        print("First number must be less than second number")
-        return False, None
-    if (nums[0] or nums[1]) < lower or (nums[0] or nums[1]) > upper:
-        print("Numbers out of bounds")
-        return False, None
-    if trip_type == "ses" and nums[-1] > nums[1] - nums[0]:
-        print("Step size cannot be larger than range")
-        return False, None
-    if trip_type == "sen":
-        try:
-            nums[2] = int(nums[2])
-        except ValueError:
-            print("Number of steps must be an integer")
-            return False, None
-        if nums[2] < 2 or nums[2] > 999999:
-            print("Number of steps must be 1 < n < 999999")
-            return False, None
-        if valid_func == int and nums[2] > nums[1] - nums[0] + 1:
-            print("Number of steps cannot exceed the range for integers")
-            return False, None
-    return True, nums
-
-
-def valid_csv(prompt: str, csv_type: str, lower: float | int = float("-inf"), upper: float | int = float("inf"),
-              valid_func: Callable = None, allowed_strings: list | tuple = None, delim: str = ",", exit_string: str = "e"):
-    string = input(f"{prompt}\n('{exit_string}' to exit)\n")
-    if string == exit_string:
-        return False, "exit"
-    csv = string.split(delim)
-    if len(csv) < 2:
-        print("You must enter at least 2 items")
-        return False, None
-    if csv_type == "nums":
-        for i, string in enumerate(csv):
-            try:
-                csv[i] == valid_func(string)
-            except ValueError:
-                print("You must enter either integers or floats")
-                return False, None
-            if csv[i] < lower or csv[i] > upper:
-                print(f"Number '{csv[i]}' out of bounds")
-                return False, None
-    if allowed_strings:
-        for string in csv:
-            if string not in allowed_strings:
-                print(f"Disallowed string '{string}' entered")
-                return False, None
-    return True, csv
 
 
 def confirm(prompt: str = "Are you sure? [y,n]",
