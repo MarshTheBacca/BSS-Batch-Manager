40388dd1775256ef7a593c105d1aa9f7f2423cf5
MarshTheBacca <marshallhunt@live.com>
2024-03-21 20:51:42 +0000

    replace os's scandir with pathlib's iterdir. Removed initialise as this applied to legacy project

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,23 +1,16 @@
-from os import scandir
 from pathlib import Path
 
 
-def fast_scandir(path: Path) -> list:
-    subfolders = [entry.path for entry in scandir(path) if entry.is_dir()]
+def fast_scandir(path: Path) -> list[Path]:
+    """
+    Recursively scan a directory for all subfolders
+
+    Args:
+        path: Path: The path to scan
+    Returns:
+        list[Path]: A list of all subfolders
+    """
+    subfolders = [path for path in Path.iterdir(path) if path.is_dir()]
     for subfolder in subfolders:
         subfolders.extend(fast_scandir(subfolder))
     return subfolders
-
-
-def initialise(output_path: Path = Path.cwd().joinpath("output_files")) -> None:
-    cwd = Path.cwd()
-    required_dirs = (output_path.joinpath("netmc"), cwd.joinpath("batches", "netmc"),
-                     output_path.joinpath("triangle_raft"), cwd.joinpath("batches", "triangle_raft"),
-                     output_path.joinpath("netmc_pores"), cwd.joinpath("batches", "netmc_pores"))
-    required_files = (cwd.joinpath("batch_history.txt"),)
-    for directory in required_dirs:
-        if not directory.exists():
-            directory.mkdir(parents=True)
-    for file in required_files:
-        if not file.exists():
-            file.touch()
