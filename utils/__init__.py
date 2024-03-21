1c7bb3d896feb94e61dd19fda06e91e340ffe1b6
MarshTheBacca <marshallhunt@live.com>
2024-03-21 20:50:03 +0000

    removed old helper functions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,4 @@
-# from .array_utils import import_2d, export_2d, print_table, remove_blanks, converter, import_batches, batch_table, export_batches
 from .array_utils import get_config_options
 from .validation_utils import get_valid_int
-# from .file_utils import fast_scandir, initialise
-# from .ssh_utils import command_print, command_lines, ssh_login
-# from .validation_utils import valid_int
-# from .other_utils import clean_name, find_char_indexes, get_batch_IDs
-# from .batch import Batch
-# from .config import Config
+from .other_utils import clean_name
 from .lammps_netmc_input_data import LAMMPSNetMCInputData, StructureType, BondSelectionProcess
