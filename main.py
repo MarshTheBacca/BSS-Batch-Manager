ac13777a81b767c29a5631d6d73454ed3997f87c
MarshTheBacca <marshallhunt@live.com>
2024-03-21 20:49:51 +0000

    completely re-written. Can now only edit a batch template and generate batches. No functionality to submit batches currently

--- main.py
+++ main.py
@@ -1,127 +1,51 @@
+from utils import LAMMPSNetMCInputData, get_valid_int
 from pathlib import Path
-from datetime import datetime
-from tzlocal import get_localzone
 
-from utils import get_config_options, initialise
-from utils import valid_int
-from utils import print_table, import_batches, batch_table, export_batches
-from utils import Config
-
-program_type_constants = {1: {"program_name": "netmc",
-                              "print_name": "NetMC"},
-                          2: {"program_name": "netmc_pores",
-                              "print_name": "NetMC Pores"},
-                          3: {"program_name": "triangle_raft",
-                              "print_name": "Triangle Raft"}}
-
-number_orders = {1: "first", 2: "second", 3: "thrid", 4: "fourth", 5: "fifth", 6: "sixth",
-                 7: "seventh", 8: "eigth", 9: "nineth", 10: "tenth"}
-
-# Notes for when u return:
-# Way of scanning output files for missing files?
-# Way of generating grahpics as .svg files automatically
-# Extract time to complete batch?
-# Extract % success/failiure of batch?
-
-# Windows issue with multiprocessing, once batch is submitted, the integer menu seems to duplicate
-# How can we figure out whether to use 'python' or 'python3' for commands on different platforms?
-# There is currenty no validation on username or output_files path in config.csv
-
-
-def get_local_constants(cwd, program_type):
-    global program_name
-    global input_path, output_path, batch_submit_script_path
-
-    # Program type constants, 1 = NetMC, 2 = Triangle Raft
-    program_name = program_type_constants[program_type]["program_name"]
-
-    # Local constants
-    input_path = cwd.joinpath("batches", program_name)
-    batch_submit_script_path = cwd.joinpath("utils", "batch_submit.py")
+NUMBER_ORDERS = {1: "first", 2: "second", 3: "third", 4: "fourth", 5: "fifth", 6: "sixth", 7: "seventh", 8: "eighth",
+                 9: "ninth", 10: "tenth"}
 
 
 def main():
-    cwd = Path(__file__).parent
-    coulson_username, output_path = get_config_options(cwd.joinpath("config.csv"), [0, 1], cwd)
-    initialise(output_path)
-    local_tz = get_localzone()
-    local_tz_string = datetime.now(local_tz).tzname()
+    common_files_path = Path(__file__).parent.joinpath("common_files")
+    batches_path = Path(__file__).parent.joinpath("batches")
+    template_data = LAMMPSNetMCInputData.from_file(common_files_path.joinpath("lammps_netmc_template.inpt"))
     while True:
-        option = valid_int("What would you like to do?\n1) Generate Batches\n2) Submit Batches to Coulson\n"
-                           "3) Analyse Batches\n4) Exit\n", 1, 4)
+        option = get_valid_int("What would you like to do?\n1) Create a batch\n"
+                               "2) Edit the batch template\n3) Submit batch to Coulson\n4) Exit\n", 1, 4)
         if option == 1:
-            while True:
-                option = valid_int("What type of program would you like to create batches for?"
-                                   "\n1) NetMC\n2) NetMC Pores\n3) Triangle Raft\n4) Exit\n", 1, 4)
-                if option == 4:
+            num_vars = get_valid_int("How many variables would you like to vary? (10 to exit)\n", 1, 10)
+            if num_vars == 10:
+                continue
+            exit_num = len(template_data.table_relevant_variables) + 1
+            selected_var_indexes = []
+            vary_arrays = []
+            cancelled = False
+            while len(selected_var_indexes) < num_vars:
+                template_data.table_print(relevant_only=True)
+                option = get_valid_int(f"Enter the {NUMBER_ORDERS[len(selected_var_indexes) + 1]} variable ({exit_num} to exit)\n", 1, exit_num)
+                if option == exit_num:
+                    cancelled = True
                     break
-                else:
-                    get_local_constants(cwd, option)
-                    dims = valid_int("How many variables would you like to vary (10 to exit)\n", 1, 10)
-                    if dims != 10:
-                        while True:
-                            config = Config(program_name)
-                            config_table, var_indexes = config.table()
-                            print_table(config_table, max_col_lengths=[30, 50, 50, 38],
-                                        headers=("Value", "Description", "Type", "Allowed Values"))
-                            exit_option = len(config_table) + 1
-                            print(f'{exit_option}) Exit\n\nThe current values in the template are given above'
-                                  f'\nPlease edit the text file: {program_name}_template.csv in the "common_files" directory to change this\n')
-                            changing_var_arrays, chosen_var_indexes = [], []
-                            breakout, count, num_jobs = 0, 0, 1
-                            while count < dims:
-                                if dims == 1:
-                                    question_string = "Which variable would you like to change\n"
-                                else:
-                                    question_string = f"Enter the {number_orders[count + 1]} variable you'd like to change\n"
-                                var_option = valid_int(question_string, 1, exit_option)
-                                if var_option == exit_option:
-                                    breakout = 1
-                                    break
-                                else:
-                                    var = config.variables[var_indexes[var_option - 1]]
-                                    print(f"You have selected: {var.name}")
-                                    not_exited, changing_var_array = var.get_cva()
-                                    if not_exited:
-                                        changing_var_arrays.append(changing_var_array)
-                                        chosen_var_indexes.append(var_indexes[var_option - 1])
-                                        num_jobs = num_jobs * len(changing_var_array)
-                                        count += 1
-                            if breakout == 1:
-                                break
-                            else:
-                                batch_name = input("Please enter a batch name for the folder these input files will be in\n")
-                                config.generate_batch(changing_var_arrays, chosen_var_indexes, input_path, batch_name)
-                                print(f"Successfully created batch with {num_jobs} jobs")
+                if option - 1 in selected_var_indexes:
+                    print("You have already selected this variable")
+                    continue
+                selected_var_indexes.append(option - 1)
+                print(f"You have selected {template_data.table_relevant_variables[option - 1].name}")
+                vary_array = template_data.table_relevant_variables[option - 1].get_vary_array()
+                if vary_array is None:
+                    cancelled = True
+                    break
+                vary_arrays.append(vary_array)
+            if cancelled:
+                continue
+            template_data.generate_batch(vary_arrays, selected_var_indexes, batches_path)
+
         elif option == 2:
-            while True:
-                option = valid_int("Which program type would you like to submit?\n1) NetMC\n2) NetMC Pores\n3) Triangle Raft\n4) Exit\n", 1, 4)
-                if option == 4:
-                    break
-                else:
-                    get_local_constants(cwd, option)
-                    while True:
-                        batches = import_batches(cwd.joinpath("batch_history.txt"), output_path)
-                        batches_table = batch_table(batches, program_name, local_tz)
-                        batch_names = [batch.name for batch in batches]
-                        exit_option = len(batches_table) + 1
-                        if exit_option == 1:
-                            print("There are no batches to submit!\n")
-                            break
-                        print("\n")
-                        print_table(batches_table, headers=("Batch Name", "Number of Jobs", "Number of Times Submitted",
-                                                            "Last Time Submitted (" + local_tz_string + ")"), dims=2)
-                        print(f"{exit_option}) Exit\n")
-                        option = valid_int("Which batch would you like to submit?\n", 1, exit_option)
-                        if option != exit_option:
-                            selected_batch = batches[batch_names.index(batches_table[option - 1][0])]
-                            selected_batch.submit(coulson_username, batch_submit_script_path, output_path)
-                            export_batches("batch_history.txt", batches)
-                            print(f"\nSuccessfully submitted {selected_batch.name}!")
-                        else:
-                            break
+            template_data.edit_value_interactive()
+            template_data.export(common_files_path.joinpath("lammps_netmc_template.inpt"))
+
         elif option == 3:
-            print("Not yet implemented")
+            pass
         elif option == 4:
             break
 
