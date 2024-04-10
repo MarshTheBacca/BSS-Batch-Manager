d347970708167965eb599f8f994b88161703d572
MarshTheBacca <marshallhunt@live.com>
2024-04-10 02:38:56 +0100

    removed batch_table, as this is handled by the BatchData class, sorted imports, changed get_options with a doc string and it returns a dictionary instead

--- utils/array_utils.py
+++ utils/array_utils.py
@@ -1,8 +1,9 @@
+import os
 from datetime import datetime, timezone
 from pathlib import Path
-import os
-from .batch import Batch
 from typing import Optional
+
+from .batch import Batch
 
 
 def import_2d(path: Path, del_indexes: Optional[tuple[int, ...]] = None,
@@ -72,15 +73,12 @@ def export_batches(path: Path, batches: list) -> None:
     export_2d(path, array, col_types=["str", "str", "datetime"], datetime_format="%Y %a %d %b %H:%M:%S %Z")
 
 
-def import_batches(batch_history_path: Path, output_path: Path, batches_path: Path = Path.cwd().joinpath("batches")) -> list:
-    programs = [prog.name for prog in os.scandir(batches_path) if prog.is_dir()]
-    batches = []
-    for prog in programs:
-        prog_batches = [batch.name[:-4] for batch in os.scandir(batches_path.joinpath(prog)) if batch.name.endswith(".zip")]
-        #               0      1        2           3
-        # batches = [[batch, path, [run_times], prog_type],..]]
-        batches.extend([[batch, batches_path.joinpath(prog, f"{batch}.zip"), [], prog] for batch in prog_batches])
-    batch_history = import_2d(batch_history_path)
+def import_batches(batch_log_path: Path, output_path: Path, batches_path: Path = Path.cwd().joinpath("batches")) -> list[Batch]:
+    batch_paths = [batch for batch in batches_path.iterdir() if batch.name.endswith(".zip")]
+    # batches = [[batch, path, [run_times], prog_type],..]]
+    batch_log = import_2d(batch_log_path)
+    if not batch_log:
+        return [Batch(path.name[:4], path, []) for path in batch_paths]
     if batch_history:
         batch_history = converter(batch_history, data_types=("str", "str", "datetime"), datetime_format="%Y %a %d %b %H:%M:%S %Z")
         for log in batch_history:
@@ -89,22 +87,7 @@ def import_batches(batch_history_path: Path, output_path: Path, batches_path: Pa
                 batches.append([log[0], "deleted", [log[2]], log[1]])
             else:
                 batches[batch_names.index(log[0])][2].append(log[2])
-    batches = [Batch(*(batch + [Path(output_path)])) for batch in batches]
-    return batches
-
-
-def batch_table(batches: list[Batch], prog_type: str, t_zone: timezone) -> list[list]:
-    """
-    Creates a table of batch data
-    Args:
-        batches: The list of batches to be displayed
-        prog_type: The type of program to be displayed
-        t_zone: The timezone to be used
-    Returns:
-        A 2D list of the batch data
-    """
-    batches.sort(reverse=True)
-    return [batch.convert_to_array(t_zone) for batch in batches if batch.type == prog_type and batch.path != "deleted"]
+    return [Batch(*(batch + [Path(output_path)])) for batch in batches]
 
 
 def converter(raw_data: list[list[str]], data_types: Optional[list[str] | tuple[str, ...]] = None,
@@ -149,13 +132,15 @@ def remove_blanks(array: list[list]) -> list[list]:
     return [element for element in array if element != []]
 
 
-def get_config_options(config_path: Path, indexes: list, cwd: Path) -> list:
-    default_mappings = {"os.get_login": os.getlogin(), "None": None, "get_output_path": cwd.joinpath("output_files")}
+def get_options(config_path: Path) -> dict:
+    """
+    Reads a config file and returns user-defined options, with default values if not specified
+
+    Args:
+        config_path (Path): The path to the config file
+    Returns:
+        A dictionary of the options
+    """
+    default_mappings = {"os.get_login": os.getlogin(), "None": None}
     config = import_2d(config_path, del_indexes=(0,))
-    return_list = []
-    for i in indexes:
-        if config[i][1]:
-            return_list.append(config[i][1])
-        else:
-            return_list.append(default_mappings[config[i][2]])
-    return return_list
+    return {option[0]: option[2] if option[2] else default_mappings.get(option[1]) for option in config}
