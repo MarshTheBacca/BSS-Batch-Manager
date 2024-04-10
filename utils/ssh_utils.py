5471357c9a32f268a1866b373094e2cc3d53afcf
MarshTheBacca <marshallhunt@live.com>
2024-04-10 02:45:52 +0100

    renamed 'mkdir_p' to land_directory (as you change to that directory too). Added doc strings, type hinting, sorted imports, removed ssh_login, now only uses ssh_login_silent (no need for maskpass either)

--- utils/ssh_utils.py
+++ utils/ssh_utils.py
@@ -1,13 +1,27 @@
 import paramiko
-import maskpass
+from pathlib import Path
 
-from .validation_utils import valid_str
+COULSON_HOSTNAME = "coulson.chem.ox.ac.uk"
 
-COULSON_HOSTNAME = COULSON_HOSTNAME
+
+class LogInException(Exception):
+    pass
 
 
 def command_print(ssh: paramiko.SSHClient, command: list) -> None:
-    _, stdout, stderr = ssh.exec_command(command)
+    """
+    Prints the output of a command run on a remote server
+
+    Args:
+        ssh (paramiko.SSHClient): The SSH client
+        command (list): The command to run
+    Raises:
+        paramiko.SSHException: If the command fails to run
+    """
+    try:
+        _, stdout, stderr = ssh.exec_command(command)
+    except paramiko.SSHException:
+        raise paramiko.SSHException(f"Error running command: {command}")
     out_lines = stdout.read().decode("ascii").split("\n")[:-1]
     for line in out_lines:
         print(line)
@@ -17,12 +31,36 @@ def command_print(ssh: paramiko.SSHClient, command: list) -> None:
 
 
 def command_lines(ssh: paramiko.SSHClient, command: str) -> list[str]:
-    _, stdout, _ = ssh.exec_command(command)
+    """
+    Gets the output of a command run on a remote server as a list of lines
+
+    Args:
+        ssh (paramiko.SSHClient): The SSH client
+        command (str): The command to run
+    Returns:
+        list[str]: The output of the command as a list of lines
+    Raises:
+        paramiko.SSHException: If the command fails to run
+    """
+    try:
+        _, stdout, _ = ssh.exec_command(command)
+    except paramiko.SSHException:
+        raise paramiko.SSHException(f"Error running command: {command}")
     lines = stdout.read().decode("ascii").split("\n")[:-1]
     return lines
 
 
-def create_ssh_client(username: str, port: int = 22, hostname: str = COULSON_HOSTNAME) -> paramiko.SSHClient:
+def create_ssh_client(username: str, hostname: str = COULSON_HOSTNAME, port: int = 22) -> paramiko.SSHClient:
+    """
+    Creates an SSH client by loading the system host keys and setting the missing host key policy to AutoAddPolicy
+
+    Args:
+        username (str): The username to login with
+        port (int): The port to connect to
+        hostname (str): The hostname to connect to
+    Returns:
+        paramiko.SSHClient: The SSH client
+    """
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -30,31 +68,61 @@ def create_ssh_client(username: str, port: int = 22, hostname: str = COULSON_HOS
     return client
 
 
-def ssh_login(username: str = None, password: str = None) -> tuple:
+def ssh_login_silent(username: str, hostname: str = COULSON_HOSTNAME) -> paramiko.SSHClient:
+    """
+    Logs into a remote server using SSH on port 22
+
+    Args:
+        username (str): The username to login with
+        hostname (str): The hostname to connect to (default is COULSON_HOSTNAME)
+    Returns:
+        paramiko.SSHClient: The SSH client
+    Raises:
+        LogInException: If the login fails
+    """
     try:
-        ssh = create_ssh_client(hostname=COULSON_HOSTNAME, port="22", username=username, password=password)
+        return create_ssh_client(username, hostname)
     except (paramiko.AuthenticationException, paramiko.SSHException):
-        while True:
-            username = valid_str("Enter your username\n", length_range=(2, 20), char_types="ASCII", exit_string="e")
-            if not username:
-                return False, None, None
-            try:
-                ssh = create_ssh_client(hostname=COULSON_HOSTNAME, port=22, username=username, password=password)
-                return ssh, username, None
-            except (paramiko.AuthenticationException, paramiko.SSHException):
-                pass
-            password = maskpass.advpass(prompt="Please enter your password for Coulson\n", mask="*")
-            try:
-                ssh = create_ssh_client(hostname=COULSON_HOSTNAME, port=22, username=username, password=password)
-                break
-            except paramiko.AuthenticationException:
-                print("Incorrect username or password.")
-    return ssh, username, password
+        raise LogInException(f"Failed to login to {hostname} as {username} on port 22")
 
 
-def ssh_login_silent(host: str, username: str = None, password: str = None) -> paramiko.SSHClient | bool:
+def sftp_exists(sftp: paramiko.SFTPClient, path: Path) -> bool:
+    """
+    Checks if a file or directory exists on the remote server
+    returns True if it does, False if it does not
+    """
     try:
-        ssh = create_ssh_client(hostname=host, port=22, username=username)
-        return ssh
-    except (paramiko.AuthenticationException, paramiko.SSHException):
+        sftp.stat(path)
+        return True
+    except FileNotFoundError:
         return False
+
+
+def land_directory(sftp: paramiko.SFTPClient, remote_path: Path) -> None:
+    """
+    Makes a directory and all parent directories on the remote server and changes to that directory
+
+    Args:
+        sftp: An open sftp connection
+        remote_path: The path to the directory to be made
+    Raises:
+        IOError: If the directory could not be made
+    Returns:
+        None
+    """
+    if remote_path == "/":
+        sftp.chdir("/")
+        return
+    if remote_path == "":
+        return
+    try:
+        sftp.chdir(remote_path.as_posix())
+    except IOError:
+        dirname = remote_path.parent
+        basename = remote_path.name
+        land_directory(sftp, dirname)
+        try:
+            sftp.mkdir(basename)
+            sftp.chdir(basename)
+        except IOError:
+            raise IOError(f"Could not make remote directory {remote_path}, check permissions")
