77f8c69543261e1b8d481dd9ef8d639f6c997af2
MarshTheBacca <marshallhunt@live.com>
2024-04-10 02:37:43 +0100

    added more imports used in main.py and such

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
-from .array_utils import get_config_options
+from .array_utils import get_options
 from .validation_utils import get_valid_int
 from .other_utils import clean_name
 from .lammps_netmc_input_data import LAMMPSNetMCInputData, StructureType, BondSelectionProcess
+from .ssh_utils import ssh_login_silent, LogInException
+from .batch_data import BatchData
