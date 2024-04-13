865a523e062460772907ef6943b8f45fa3cf0145
MarshTheBacca <marshallhunt@live.com>
2024-04-13 20:30:42 +0100

    removed blank file

--- test.py
+++
@@ -1,6 +0,0 @@
-from utils import LAMMPSNetMCInputData
-from pathlib import Path
-cwd = Path(__file__).parent
-input_data = LAMMPSNetMCInputData.from_file(cwd.joinpath("netmc.inpt"))
-input_data.table_print()
-input_data.export(cwd.joinpath("netmc_2.inpt"))
