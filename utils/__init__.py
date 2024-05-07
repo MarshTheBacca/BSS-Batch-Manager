f7ff34faf949b1ea063eeb7ee395d025a4859a1e
MarshTheBacca <marshallhunt@live.com>
2024-05-07 13:44:53 +0100

    added receive_batches

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@ from .batch_data import BatchData
 from .bss_input_data import BSSInputData
 from .custom_types import BSSType
 from .other_utils import clean_name, generate_job_name, select_path, select_network, select_potential, get_batch_name
-from .ssh_utils import LogInException, ssh_login_silent
+from .ssh_utils import LogInException, ssh_login_silent, receive_batches
 from .validation_utils import confirm, get_valid_int, get_valid_str
 from .var import Var
 from .bss_output_data import BSSOutputData
